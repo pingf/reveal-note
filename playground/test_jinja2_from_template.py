9b3236c0cb1801ac37a03044f73c5d9b879b4f0f
fanchao-meng <fcmeng@thoughtworks.com>
2015-03-30 22:18:30 +0530

    delete venv

--- playground/test_jinja2_from_template.py
+++ playground/test_jinja2_from_template.py
@@ -4,4 +4,8 @@ if __name__ == '__main__':
     env = Environment(loader=PackageLoader('test_pack', 'templates'))
     template = env.get_template('template.html')
     r = template.render(name='Jesse')
+    generate = 'generate.html'
+    f = open(generate, 'w')
+    f.write(r)
+    f.close()
     print(r)
